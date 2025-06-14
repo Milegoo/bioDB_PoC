e81787a588ca67cb1bfaa7b4e41566de7c297a16
Eloi Milego <127399679+Milegoo@users.noreply.github.com>
2025-06-14 18:11:20 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ const config = {
     serverMode: "full", // file is just a plain old full sqlite database
     requestChunkSize: 4096, // the page size of the  sqlite database (by default 4096)
     url: "/bioDB_PoC/pocBioDB.db" // url to the database (relative or full)
+    filesize: 36864
   }
 };
 
